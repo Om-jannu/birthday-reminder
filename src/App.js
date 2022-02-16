1045ebaf1b02a1945dfc92ebda2f6385a6ace520
Om_jannu <omjannu2002@gmail.com>
2022-02-16 23:58:51 +0530

    casul commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-
-function App() {
+import React from "react";
+import home from "./components/Home";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" element={home}/>
+    </Router>
   );
-}
+};
 
 export default App;
